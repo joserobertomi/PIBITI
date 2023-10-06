25a90fa184c81ba488c847d527de2f4c450e74e9
jrcodee <joserobertomi1203@gmail.com>
2023-10-06 11:10:15 -0300

    suncionando  por controle

--- script_motor/main.py
+++ script_motor/main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as gpio
 import time
+import getch
 
 def init():    
     gpio.setmode(gpio.BCM)
@@ -17,7 +18,8 @@ def frente(sec): # OKOK
     time.sleep(sec)
     gpio.cleanup() 
     
-def re(sec): # OKOK
+def re(sec): #PauPau
+    init()
     gpio.output(17, False)
     gpio.output(22, True)
     gpio.output(23, False)
@@ -34,7 +36,7 @@ def giro_antih(sec): # OKOK
     time.sleep(sec)
     gpio.cleanup()
     
-def giro_h(sec): # OKOK
+def giro_h(sec): # PauPau
     init()
     gpio.output(17, True)
     gpio.output(22, False)
@@ -52,12 +54,22 @@ def parab_esquerda(sec): # OKOK
     time.sleep(sec)
     gpio.cleanup()
     
-def parab_direita(sec): # OKOK
+def parab_direita(sec): #OKOK
     init()
     gpio.output(17, True)
     gpio.output(22, False)
     gpio.output(23, False)
     gpio.output(24, False)
+    time.sleep(sec)
+    gpio.cleanup()
+    
+    
+def manuntencao(sec): 
+    init()
+    gpio.output(17, False)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    gpio.output(24, True)
     time.sleep(sec)
     gpio.cleanup()
 
@@ -123,9 +135,50 @@ def comando(seconds):
             #time.sleep(seconds+2)
         else: 
             print("Comando invalido")
-        
+            
+def controle_pelo_computador():    
+    seconds = 0.025
+    a = ''
+    while a != 'x' :
+        a = get_single_char()
+        print()
+        if a is not None: 
+            if a == "w":
+                #print("Frente")
+                frente(seconds)
+                #time.sleep(seconds+2)
+            elif a == "s":
+                #print("Re")
+                re(seconds)
+                #time.sleep(seconds+2)
+            elif a == "d":    
+                #print("Giro horario")
+                giro_h(seconds)
+                #time.sleep(seconds+2)
+            elif a == "a":
+                #print("Giro anti-horario")
+                giro_antih(seconds)
+                #time.sleep(seconds+2)
+            elif a == "q":    
+                #print("Parabola esquerda")
+                parab_esquerda(seconds)
+                #time.sleep(seconds+2)
+            elif a == "e":   
+                #print("Parabola direita")
+                parab_direita(seconds)
+                #time.sleep(seconds+2)
+            #else: 
+                #print("Comando invalido")
+    print("Precionou 'x', saindo do modo de controle")
+
+def get_single_char():
+    try:
+        char = getch.getch()
+        return char
+    except KeyboardInterrupt:
+        return None
 
 if __name__ == '__main__':
-    comando(1)
+    controle_pelo_computador()
     print('Arreguei')
     
