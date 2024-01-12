ef6065f42bc07fc0c08202c30134f65e0010c882
mxtqnt <anacarolinass.cc@gmail.com>
2024-01-12 15:23:15 -0300

    Fix: Return %

--- camera/main.py
+++ camera/main.py
@@ -1,17 +1,18 @@
 import numpy as np
 import cv2 as cv
+import time
 
-def contar_pixels(area):
-    brancos = np.sum(area == 255)
-    pretos = np.sum(area == 0)
+def contar_pixels(area, total):
+    brancos = np.sum(area == 255) * 100 // total
+    pretos = np.sum(area == 0) * 100 // total
     return pretos, brancos
 
 def capturar_video(cap, x, y, largura, altura, x2, y2):
-
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
     while True:
+        time.sleep(1)
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
@@ -19,16 +20,18 @@ def capturar_video(cap, x, y, largura, altura, x2, y2):
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         retval, threshold = cv.threshold(gray, 50, 255, cv.THRESH_BINARY)
 
+        total = largura * altura
         roi = threshold[y:y+altura, x:x+largura]
         roi2 = threshold[y2:y2+altura, x2:x2+largura]
-        pretos, brancos = contar_pixels(roi)
-        pretos2, brancos2 = contar_pixels(roi2)
+
+        pretos, brancos = contar_pixels(roi, total)
+        pretos2, brancos2 = contar_pixels(roi2, total)
 
         frame = cv.rectangle(frame, (x, y), (x+largura, y+altura), (255, 0, 255), 1) 
-        frame = cv.putText(frame, ('P:' + str(pretos) + ' B:' + str(brancos)), (x, y + altura + 10), cv.FONT_HERSHEY_SIMPLEX , 0.3, (255, 0, 255), 1, cv.LINE_AA) 
+        frame = cv.putText(frame, ('P:' + str(pretos) + '% B:' + str(brancos) + "%"), (x, y + altura + 10), cv.FONT_HERSHEY_SIMPLEX , 0.3, (255, 0, 255), 1, cv.LINE_AA) 
 
         frame = cv.rectangle(frame, (x2, y2), (x2+largura, y2+altura), (255, 0, 255), 1) 
-        frame = cv.putText(frame, ('P:' + str(pretos2) + ' B:' + str(brancos2)), (x2, y2 + altura + 10), cv.FONT_HERSHEY_SIMPLEX , 0.3, (255, 0, 255), 1, cv.LINE_AA) 
+        frame = cv.putText(frame, ('P:' + str(pretos2) + '% B:' + str(brancos2) + "%"), (x2, y2 + altura + 10), cv.FONT_HERSHEY_SIMPLEX , 0.3, (255, 0, 255), 1, cv.LINE_AA) 
 
         cv.imshow('frame', frame)
         if cv.waitKey(1) == ord('q'):
@@ -49,4 +52,5 @@ if __name__ == '__main__':
     largura, altura, margem = 50, 50, 50
     x, y =  margem, height//2
     x2, y2 =  width - largura - margem, height//2
+
     capturar_video(cap, x, y, largura, altura, x2, y2)
