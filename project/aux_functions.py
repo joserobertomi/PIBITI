64b1f2a8a230ad7de8d5f1a094e87d47004eee71
joserobertomi <joserobertomi@outlook.com>
2024-05-06 13:01:59 -0300

    add new function

--- project/aux_functions.py
+++ project/aux_functions.py
@@ -5,10 +5,15 @@ def print_aim_result(result):
 def take_move_decision(base, potencia_max): 
     if not (base[2]+base[6]+base[0]+base[8]):
         return potencia_max*0.75, potencia_max*0.75
-    return ((base[2]+base[6])/3)*potencia_max, ((base[0]+base[8])/3)*potencia_max
+    elif base[2]+base[6]+base[0]+base[8] == 1:
+        return ((base[2]+base[6])/3)*potencia_max+potencia_max/3, ((base[0]+base[8])/3)*potencia_max+potencia_max/3
+        
+    return ((base[2]+base[6])/6)*potencia_max, ((base[0]+base[8])/6)*potencia_max
 
 
 print(take_move_decision([0,0,0,0,0,0,0,0,0], 100))
 print(take_move_decision([0,1,0,0,1,0,0,1,0], 100))
 print(take_move_decision([1,1,0,0,1,0,0,1,1], 100))
 print(take_move_decision([0,1,1,0,1,0,1,1,0], 100))
+print(take_move_decision([0,1,0,0,1,0,0,1,1], 100))
+print(take_move_decision([0,1,1,0,1,0,0,1,0], 100))
