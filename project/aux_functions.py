ddfb6055d0d6138b0a0c7788eb9e3395fab41380
joserobertomi <joserobertomi@outlook.com>
2024-05-06 12:46:32 -0300

    add movimentation decision

--- project/aux_functions.py
+++ project/aux_functions.py
@@ -1,12 +1,14 @@
-import numpy as np
-
 def print_aim_result(result):
     print(f'-------------------\n|  {result[0]}  |  {result[1]}  |  {result[2]}  |\n|  {result[3]}  |  {result[4]}  |  {result[5]}  |\n|  {result[6]}  |  {result[7]}  |  {result[8]}  |\n-------------------')
     
     
-def take_move_decision(base): 
-    a = np.array([(base[0], base[1], base[2]), (base[3], base[4], base[5]), (base[6], base[7], base[8])])
-    if np.sum(a[:,1]) == 3:
-        stt = 'Tenho uma reta'
-        a[:,0] = a[:,0] * (-1)
-        print(a) 
+def take_move_decision(base, potencia_max): 
+    if not (base[2]+base[6]+base[0]+base[8]):
+        return potencia_max*0.75, potencia_max*0.75
+    return ((base[2]+base[6])/3)*potencia_max, ((base[0]+base[8])/3)*potencia_max
+
+
+print(take_move_decision([0,0,0,0,0,0,0,0,0], 100))
+print(take_move_decision([0,1,0,0,1,0,0,1,0], 100))
+print(take_move_decision([1,1,0,0,1,0,0,1,1], 100))
+print(take_move_decision([0,1,1,0,1,0,1,1,0], 100))
