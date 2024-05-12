ff524d7ffa39cb5192ffc7801570e9bb5f2afa02
joserobertomi <joserobertomi@outlook.com>
2024-05-12 12:02:54 -0300

    updates to test

--- project/main.py
+++ project/main.py
@@ -1,5 +1,5 @@
-from imagem import encontrar_linha, threshold_colorido
-from direcionamento import vetor_central, posicionar_mira
+from imagem import encontrar_linha, threshold_colorido, vetor_central, posicionar_mira_9x9
+from direcionamento import maximizacao
 from movimentacao import parar, frente
 from raspi_camera import init_camera, take_photo
 from aux_functions import print_aim_result, take_move_decision
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     print('Hello Pi 3!')
     cam_size=(640, 480) # define por padrao o tamanho da camera
-    tamanho = 100 # Definir o tamanho dos segmentos da mira
+    tamanho = 20 # Definir o tamanho dos segmentos da mira
     camera = init_camera(cam_size=cam_size) # inicia a camera pelo modulo do raspi
     
     c = 5
@@ -29,13 +29,19 @@ if __name__ == '__main__':
         
         vetor_centro = vetor_central(treated_frame, cam_size[1], cam_size[0])
         centro_mira = encontrar_linha(vetor_centro)
-
-        aimmed_frame, aim_result = posicionar_mira(threshold_colorido(treated_frame), centro_mira, tamanho, cam_size[1])
+        
+        framecolorido = threshold_colorido(treated_frame)
+        aim_result = posicionar_mira_9x9(cam_size[0]//2, cam_size[1]//2, tamanho, treated_frame, framecolorido)
+        soma, somaesquerda, somadireita = maximizacao(aim_result)
+    
+        framecolorido = cv.putText(framecolorido, ('soma: ' + str(soma)),                 (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1, cv.LINE_AA) 
+        framecolorido = cv.putText(framecolorido, ('somaesquerda: ' + str(somaesquerda)), (20, 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1, cv.LINE_AA) 
+        framecolorido = cv.putText(framecolorido, ('somadireita: ' + str(somadireita)),   (20, 60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1, cv.LINE_AA) 
 
         new_path ="/home/perry/project/images/aimmed-frame.png"
-        cv.imwrite(new_path, aimmed_frame)
+        cv.imwrite(new_path, framecolorido)
                 
-        #print_aim_result(aim_result)
+        print_aim_result(aim_result)
         
         pot_e, pot_d = take_move_decision(aim_result, 80)
         print(f'Potencia Esq: {pot_e} | Potencia Dir: {pot_d}')
